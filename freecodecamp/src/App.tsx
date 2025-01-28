37222fc720b7bceb7e6b7401712bca4a9d6178cb
Arch-Daryhen <daryhen.work@gmail.com>
2025-01-28 01:12:28 -0500

    changed app.tsx

--- freecodecamp/src/App.tsx
+++ freecodecamp/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline bg-lime-600">Hello world!</h1>
     </>
   );
 }
